eef676b4f3269527e6dde4b1368c89fcb0d1e055
iwkt <iwk@mac.com>
2021-09-24 06:45:58 +0900

    sidebar menu test

--- sidebar.php
+++ sidebar.php
@@ -2,33 +2,45 @@
   <button id="MenuBtn" class="p-sidebar__btn-close"><span class="c-close-btn"></span></button>
   <div id="foodMenu">
     <!-- <ul class="p-sidebar__menu"> -->
-      <!-- <li v-for="item in foodItems" :key="item.sn">
+    <!-- <li v-for="item in foodItems" :key="item.sn">
         <h3 class="p-sidebar__mainmenu-item">{{ item.category }}</h3>
         <ul class="p-sidebar__submenu-items">
           <li class="p-sidebar__submenu-item"> -->
-            <nav class="globalNavi">
-              <?php
-              $args = array(
-                'menu'        => 'gmenu',
-                'menu_class'  => 'p-sidebar__menu',
-                'container' => false,
-              );
-              wp_nav_menu($args);
-              ?>
-            </nav>
+    <nav class="globalNavi">
+      <?php
+      $args = array(
+        'menu'        => 'gmenu',
+        'menu_class'  => 'p-sidebar__menu',
+        'container' => false,
+      );
+      wp_nav_menu($args);
+      ?>
+    </nav>
 
-        <!-- こちらはカスタムカテゴリ -->
-            <nav class="globalNavi">
-              <?php
-              $args = array(
-                'menu'        => 'global-navigation-custom',
-                'menu_class'  => 'p-sidebar__menu',
-                'container' => false,
-              );
-              wp_nav_menu($args);
-              ?>
-            </nav>
-          <!-- </li>
+    <!-- こちらはカスタムカテゴリ -->
+    <nav class="globalNavi">
+      <?php
+      $args = array(
+        'menu'        => 'global-navigation-custom',
+        'menu_class'  => 'p-sidebar__menu',
+        'container' => false,
+      );
+      wp_nav_menu($args);
+      ?>
+    </nav>
+
+    <!-- こちらはカスタムカテゴリ -->
+    <nav class="globalNavi">
+      <?php
+      $args = array(
+        'menu'        => 'item_menu',
+        'menu_class'  => 'p-sidebar__menu',
+        'container' => false,
+      );
+      wp_nav_menu($args);
+      ?>
+    </nav>
+    <!-- </li>
         </ul>
       </li> -->
     <!-- </ul> -->
