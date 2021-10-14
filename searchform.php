17410d9fc6fd41b48718ce03f2c18cb005c1bd23
iwkt <iwk@mac.com>
2021-10-14 23:54:45 +0900

    seaarch-form修正

--- searchform.php
+++ searchform.php
@@ -1,11 +1,8 @@
 <form id="searchform" class="p-search-form " method="get" action="<?php echo esc_url(home_url('/')); ?>">
-  <label for="search">
-    <div class="p-search__wrapper">
-      <div class="p-search__field-wrapper mr">
-        <span class="c-icon-btn c-beforeicon-btn--download"></span>
-        <input class="p-search-field" type="search" name="s" id="s">
-      </div>
-      <input class="c-gradietion-btn" type="submit" name="submit" value="検索">
-    </div>
-  </label>
+  <div class="p-search__wrapper">
+    <input class="p-search-field" type="search" name="s" id="s">
+    <icon type="submit" class="p-search-icon"><i class="fas fa-search"></i></icon>
+    <!-- </div> -->
+    <input class="c-gradietion-btn" type="submit" name="submit" value="検索">
+  </div>
 </form>
